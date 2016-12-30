8ba50c97a5eee90910bd4b5cd1e339057aa31cf7
Yousef Shanawany <askyous@gmail.com>
2016-12-29 20:09:15 -0800

    Fixed project in project weird structure

---
+++ api/controllers/PetUserController.js
@@ -0,0 +1,11 @@
+/**
+ * PetUserController
+ *
+ * @description :: Server-side logic for managing Petusers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/PetUser.js
@@ -0,0 +1,17 @@
+/**
+ * PetUser.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+ module.exports = {
+   attributes: {
+     owner:{
+       model:'user'
+     },
+     pet: {
+       model: 'pet'
+     }
+   }
+ }
